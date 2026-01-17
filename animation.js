2e05c83248013571033ead585a61990bff2ef890
tharejarehan0-a11y <tharejarehan0@gmail.com>
2026-01-17 17:31:03 +0530

    worked on the design

---
+++ animation.js
@@ -0,0 +1,34 @@
+let collection = document.getElementsByClassName("hide");
+let plane=document.getElementById("plane");
+let trail = document.getElementById("trail");
+let arr = [...collection,plane,trail]
+arr.forEach(element => {
+    element.style.opacity=0;
+    setTimeout(() => {
+        element.style.opacity=1;
+    }, 5000);
+});
+setTimeout(()=>{
+    setInterval(()=>{
+        let fonts = ['Roboto','Poppins','Pacifico','Oswald','Pinyon Script','Limelight','Fascinate'];
+        let randomNumber = Math.floor(Math.random()*7);
+        let fontUsed = fonts[randomNumber]
+        let letters = ['A','M','D','R','A','P'];
+        let text = document.getElementById('name');
+        text.innerHTML="";
+        let htm = text.innerHTML
+        text.style.width="30px";
+        let i = 0;
+        text.style.fontFamily=`${fontUsed}`;
+        const textAnimation = setInterval(()=>{
+            if(i==6){
+               clearInterval(textAnimation);
+            }
+            else{
+               text.innerHTML += letters[i];
+               i++
+            }
+        },300)
+    },3000)
+},8000);
+
